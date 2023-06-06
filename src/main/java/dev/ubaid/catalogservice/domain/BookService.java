9fc49385e93df3dab14befb69ea3befb6b9266f7
ubaid4j <ubaidshan007@gmail.com>
2023-06-06 06:22:30 +0500

    - enhancements

--- src/main/java/dev/ubaid/catalogservice/domain/BookService.java
+++ src/main/java/dev/ubaid/catalogservice/domain/BookService.java
@@ -8,40 +8,39 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class BookService {
-    
+
     private final BookRepository bookRepository;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-    
+
     @SneakyThrows
     public Iterable<Book> viewBookList() {
-        Thread.sleep(ThreadLocalRandom.current().nextInt(6000));
         return bookRepository.findAll();
     }
-    
+
     public Book viewBookDetails(String isbn) {
         return bookRepository.findByIsbn(isbn)
             .orElseThrow(() -> new BookNotFoundException(isbn));
     }
-    
+
     public Book addBookToCatalog(Book book) {
         if (bookRepository.existsByIsbn(book.isbn())) {
             throw new BookAlreadyExistsException(book.isbn());
         }
         return bookRepository.save(book);
     }
-    
+
     public void removeBookFromCatalog(String isbn) {
         bookRepository.deleteByIsbn(isbn);
     }
-    
+
     public Book editBookDetails(String isbn, Book book) {
         return bookRepository.findByIsbn(isbn)
             .map(existingBook -> {
                 var bookToUpdate = new Book(
-                    existingBook.id(), 
+                    existingBook.id(),
                     existingBook.isbn(),
                     book.title(),
                     book.author(),
