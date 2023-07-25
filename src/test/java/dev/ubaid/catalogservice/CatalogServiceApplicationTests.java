14564fd7cfb12e96023f16f7a1706632ae0391a3
ubaid4j <ubaidshan007@gmail.com>
2023-07-25 10:31:48 +0500

    - security tests

--- src/test/java/dev/ubaid/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/dev/ubaid/catalogservice/CatalogServiceApplicationTests.java
@@ -2,6 +2,8 @@ package dev.ubaid.catalogservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import dev.ubaid.catalogservice.domain.Book;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -26,6 +29,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 class CatalogServiceApplicationTests {
     
+    private static KeycloakToken bjornToken; //is not an employee
+    private static KeycloakToken isabellaToken;
 
     @Autowired
     private WebTestClient webTestClient;
@@ -37,8 +42,8 @@ class CatalogServiceApplicationTests {
     
     @DynamicPropertySource
     private static void dynamicProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.security.oauth2.resourceserver.jwt.isuer-uri", 
-                () -> keycloakContainer.getAuthServerUrl() + "realms/PolarBookshop");
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", 
+                () -> keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop");
     }
     
     @BeforeAll
@@ -47,7 +52,8 @@ class CatalogServiceApplicationTests {
                 .baseUrl(keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop/protocol/openid-connect/token")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                 .build();
-        
+        isabellaToken = authenticateWith("isabelle", "password", webClient);
+        bjornToken = authenticateWith("bjorn", "password", webClient);
     }
     
     @Test
@@ -61,6 +67,7 @@ class CatalogServiceApplicationTests {
         webTestClient
             .post()
             .uri("/api/books")
+                .headers(headers -> headers.setBearerAuth(isabellaToken.accessToken()))
             .bodyValue(expectedBook)
             .exchange()
             .expectStatus().isCreated()
@@ -69,5 +76,47 @@ class CatalogServiceApplicationTests {
                 assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
             });
     }
+    
+    @Test
+    void whenPostRequestUnauthorizedThen403() {
+        Book book = Book.of("1234567890", "Title", "Ubaid", 11.01, "Ubaid pub");
+        webTestClient
+                .post()
+                .uri("/api/books")
+                .headers(headers -> headers.setBearerAuth(bjornToken.accessToken()))
+                .bodyValue(book)
+                .exchange()
+                .expectStatus().isForbidden();
+                
+    }
+    
+    @Test
+    void whenPostRequestUnauthenticatedThen401() {
+        Book book = Book.of("1234567890", "Title", "ubaid", 111.10, "Ubaid pub");
+        webTestClient
+                .post()
+                .uri("/api/books")
+                .bodyValue(book)
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
 
+    private static KeycloakToken authenticateWith(String username, String password, WebClient webClient) {
+        return webClient
+                .post()
+                .body(BodyInserters.fromFormData("grant_type", "password")
+                        .with("client_id", "polar-test")
+                        .with("username", username)
+                        .with("password", password))
+                .retrieve()
+                .bodyToMono(KeycloakToken.class)
+                .block();
+    }
+    
+    private record KeycloakToken(String accessToken) {
+        @JsonCreator
+        private KeycloakToken(@JsonProperty("access_token") final String accessToken) {
+            this.accessToken = accessToken;
+        }
+    }
 }
