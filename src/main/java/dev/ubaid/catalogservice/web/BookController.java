fb11ad8eda478654dc2504c7b0799ad7c3420fd9
ubaid4j <ubaidshan007@gmail.com>
2023-07-30 12:39:39 +0500

    - added logs

--- src/main/java/dev/ubaid/catalogservice/web/BookController.java
+++ src/main/java/dev/ubaid/catalogservice/web/BookController.java
@@ -3,6 +3,7 @@ package dev.ubaid.catalogservice.web;
 import dev.ubaid.catalogservice.domain.Book;
 import dev.ubaid.catalogservice.domain.BookService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/books")
 public class BookController {
     private final BookService bookService;
@@ -25,28 +27,33 @@ public class BookController {
     
     @GetMapping
     public Iterable<Book> getAll() {
+        log.info("REST request to get all books in the catalog");
         return bookService.viewBookList();
     }
     
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info("REST request to get book by ISBN {}", isbn);
         return bookService.viewBookDetails(isbn);
     }
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@Valid @RequestBody Book book) {
+        log.info("REST request to add book: {}", book);
         return bookService.addBookToCatalog(book);
     }
     
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("REST request to remove book from isbn {}", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
     
     @PutMapping("{isbn}")
     public Book updateOrCreate(@Valid @PathVariable String isbn, @RequestBody Book book) {
+        log.info("REST request to edit book detail: {}", book);
         return bookService.editBookDetails(isbn, book);
     }
 }
--- src/main/java/dev/ubaid/catalogservice/web/BookControllerAdvice.java
+++ src/main/java/dev/ubaid/catalogservice/web/BookControllerAdvice.java
@@ -4,6 +4,8 @@ import dev.ubaid.catalogservice.domain.BookAlreadyExistsException;
 import dev.ubaid.catalogservice.domain.BookNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,23 +14,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class BookControllerAdvice {
     
     @ExceptionHandler(BookNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String bookNotFoundHandler(BookNotFoundException exp) {
+        log.error("Not found: {}", exp.getMessage());
         return exp.getMessage();
     }
     
     @ExceptionHandler(BookAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     String bookAlreadyExistsException(BookAlreadyExistsException exp) {
+        log.error("unprocessable: {}", exp.getMessage());
         return exp.getMessage();
     }
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(MethodArgumentNotValidException exp) {
+        log.error("bad request: {}", exp.getMessage());
         var errors = new HashMap<String, String>();
         exp.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
