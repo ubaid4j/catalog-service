f9327a8215496f72751f68cdb51fe727086726b8
ubaid4j <ubaidshan007@gmail.com>
2023-08-02 10:58:41 +0500

    - add observability stack

--- src/main/java/dev/ubaid/catalogservice/config/SecurityConfig.java
+++ src/main/java/dev/ubaid/catalogservice/config/SecurityConfig.java
@@ -3,10 +3,10 @@ package dev.ubaid.catalogservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -24,10 +24,11 @@ public class SecurityConfig {
         return httpSecurity
                 .authorizeHttpRequests(authorize ->
                     authorize
+                        .requestMatchers(HttpMethod.GET,"/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/api/books/**").permitAll()
                         .anyRequest().hasRole("employee")
                 )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
